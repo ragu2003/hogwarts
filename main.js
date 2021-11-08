ac4dc21c60cdbeb555bd85e660ea9f4b97fff6e8
ragu2003 <f20201726@goa.bits-pilani.ac.in>
2021-11-08 23:29:13 +0530

    Initial commit

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ btn.onclick = function(){
 searchBtn.onclick = function(){
     sidebar.classList.toggle("active");
 }
+
+let bloodoptions = document.querySelector(".blood-options-list")
+function showblood(){
+    bloodoptions.classList.toggle("activeblood");
+}
+
+function showblood1(){
+    bloodoptions.classList.toggle("activeblood");
+    document.querySelector(".SelectBloodLine").innerHTML= "PureBlood";
+}
+function showblood2(){
+    bloodoptions.classList.toggle("activeblood");
+    document.querySelector(".SelectBloodLine").innerHTML= "Halfblood";
+}
+function showblood3(){
+    bloodoptions.classList.toggle("activeblood");
+    document.querySelector(".SelectBloodLine").innerHTML= "MuggleBorn";
+}

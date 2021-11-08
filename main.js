f2e33df5acab691e5934adbc6eac9185f06d55b0
ragu2003 <f20201726@goa.bits-pilani.ac.in>
2021-11-09 01:16:26 +0530

    Dropdown

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ searchBtn.onclick = function(){
     sidebar.classList.toggle("active");
 }
 
+
+
+
 let bloodoptions = document.querySelector(".blood-options-list")
 function showblood(){
     bloodoptions.classList.toggle("activeblood");
@@ -17,13 +20,38 @@ function showblood(){
 
 function showblood1(){
     bloodoptions.classList.toggle("activeblood");
-    document.querySelector(".SelectBloodLine").innerHTML= "PureBlood";
+    document.querySelector(".SelectBloodLine-text").innerHTML= "PureBlood";
 }
 function showblood2(){
     bloodoptions.classList.toggle("activeblood");
-    document.querySelector(".SelectBloodLine").innerHTML= "Halfblood";
+    document.querySelector(".SelectBloodLine-text").innerHTML= "Halfblood";
 }
 function showblood3(){
     bloodoptions.classList.toggle("activeblood");
-    document.querySelector(".SelectBloodLine").innerHTML= "MuggleBorn";
+    document.querySelector(".SelectBloodLine-text").innerHTML= "MuggleBorn";
+}
+
+
+
+
+let houseoptions = document.querySelector(".house-options-list")
+function showhouse(){
+    houseoptions.classList.toggle("activehouse");
+}
+
+function showhouse1(){
+    houseoptions.classList.toggle("activehouse");
+    document.querySelector(".SelectHouse-text").innerHTML= "Gryffindor";
+}
+function showhouse2(){
+    houseoptions.classList.toggle("activehouse");
+    document.querySelector(".SelectHouse-text").innerHTML= "Hufflepuff";
+}
+function showhouse3(){
+    houseoptions.classList.toggle("activehouse");
+    document.querySelector(".SelectHouse-text").innerHTML= "Ravenclaw";
+}
+function showhouse4(){
+    houseoptions.classList.toggle("activehouse");
+    document.querySelector(".SelectHouse-text").innerHTML="Slytherin";
 }
